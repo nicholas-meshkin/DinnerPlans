0bac8b9aea324d72f9f526994ccd1f45b186a0e4
Nicholas Meshkin <nick.t.meshkin@gmail.com>
2024-02-14 12:22:15 -0600

    set up auth0 authentication for fe

--- DinnerPlans/Client/Program.cs
+++ DinnerPlans/Client/Program.cs
@@ -8,4 +8,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//from https://auth0.com/blog/securing-blazor-webassembly-apps/
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
