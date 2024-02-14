3c3577b6ba06b130156d18d896726bd5dc086009
Nicholas Meshkin <nick.t.meshkin@gmail.com>
2024-02-14 14:20:26 -0600

    added auth to backend, seems to be working

--- DinnerPlans/Client/Program.cs
+++ DinnerPlans/Client/Program.cs
@@ -1,18 +1,26 @@
 using DinnerPlans.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("ServerAPI",
+      client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+  .CreateClient("ServerAPI"));
 
 //from https://auth0.com/blog/securing-blazor-webassembly-apps/
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
 });
 
 await builder.Build().RunAsync();
--- DinnerPlans/Server/Program.cs
+++ DinnerPlans/Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,12 +9,56 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+//from https://auth0.com/blog/aspnet-web-api-authorization/
+//and  https://auth0.com/blog/securing-blazor-webassembly-apps/ ?
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+        options.TokenValidationParameters =
+          new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+          {
+              ValidAudience = builder.Configuration["Auth0:Audience"],
+              ValidIssuer = $"https://{builder.Configuration["Auth0:Domain"]}",
+              ValidateLifetime = true,
+          };
+    });
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Glossary", Version = "v1" });
+        var securitySchema = new OpenApiSecurityScheme
+        {
+            Description = "Using the Authorization header with the Bearer scheme.",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        };
+
+        c.AddSecurityDefinition("Bearer", securitySchema);
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+          {
+              { securitySchema, new[] { "Bearer" } }
+          });
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
@@ -27,6 +73,10 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+//from https://auth0.com/blog/aspnet-web-api-authorization/
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.MapRazorPages();
